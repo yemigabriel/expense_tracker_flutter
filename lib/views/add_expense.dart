95e0d11ce99927c8c60e6b6fdd96971155ef002f
yemi gabriel <yemig@banquemisr.com>
2025-09-08 16:45:48 +0100

    ui changes

--- lib/views/add_expense.dart
+++ lib/views/add_expense.dart
@@ -1,4 +1,8 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/view_model/expenses_view_model.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AddExpense extends StatefulWidget {
   const AddExpense({super.key});
@@ -8,11 +12,103 @@ class AddExpense extends StatefulWidget {
 }
 
 class _AddExpenseState extends State<AddExpense> {
+  late final TextEditingController _amount;
+  late final TextEditingController _description;
+  String? _selectedCategory;
+
+  @override
+  void initState() {
+    _amount = TextEditingController();
+    _description = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _amount.dispose();
+    _description.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final vm = context.read<ExpensesViewModel>();
+
     return Scaffold(
       appBar: AppBar(title: Text("Add Expense")),
-      body: const Text("Add expense"),
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          spacing: 25.0,
+          children: [
+            TextField(
+              controller: _amount,
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              decoration: InputDecoration(
+                labelText: "Amount",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            TextField(
+              controller: _description,
+              decoration: InputDecoration(
+                labelText: "Description",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            DropdownButtonFormField(
+              items: [
+                for (var category in ExpenseCategory.values)
+                  DropdownMenuItem(value: category, child: Text(category.name)),
+              ],
+              onChanged: (value) {
+                _selectedCategory = value?.name;
+              },
+              decoration: InputDecoration(
+                labelText: "Category",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: TextButton(
+                onPressed: () {
+                  final amount = double.tryParse(_amount.text) ?? 0;
+                  final description = _description.text;
+                  final expense = Expense(
+                    title: description,
+                    amount: amount,
+                    category: _selectedCategory != null
+                        ? ExpenseCategory.values.firstWhere(
+                            (e) => e.name == _selectedCategory,
+                          )
+                        : ExpenseCategory.others,
+                    date: DateTime.now(),
+                  );
+                  vm.addExpense(expense);
+                  Navigator.pop(context);
+                },
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 14, horizontal: 24),
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+
+                child: Text("Add Expense"),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,11 +1,16 @@
+import 'dart:math';
+
+import 'package:expense_tracker/view_model/expenses_view_model.dart';
 import 'package:expense_tracker/views/add_expense.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<ExpensesViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -23,89 +28,150 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 20.0,
-          children: [
-            Center(
-              child: Image.asset(
-                'assets/images/empty.png',
-                width: MediaQuery.of(context).size.width / 1.5,
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            Center(
-              child: Text(
-                "No expenses yet",
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ),
-            Center(
-              child: ElevatedButton.icon(
-                onPressed: () {
-                  showBottomSheet(context);
-                },
-                icon: Icon(Icons.add),
-                label: Text("Add your first expense"),
-                style: ElevatedButton.styleFrom(
-                  // textStyle: Theme.of(context).textTheme.labelLarge,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
+        child: viewModel.expenses.isEmpty
+            ? EmptyView(showBottomSheet: showBottomSheet)
+            : ExpenseListView(),
+      ),
+    );
+  }
+
+  void addExpense(BuildContext context) {
+    Navigator.pop(context);
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return AddExpense();
+      },
+    );
+  }
+
+  void showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: Colors.white,
+      builder: (context) {
+        return SizedBox(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Wrap(
+              children: [
+                Center(
+                  child: Container(
+                    width: 40,
+                    height: 4,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[800],
+                      borderRadius: BorderRadius.circular(2),
+                    ),
                   ),
                 ),
-              ),
+                ListTile(
+                  leading: Icon(Icons.edit),
+                  title: Text("Add expenses manually"),
+                  onTap: () => addExpense(context),
+                ),
+                ListTile(
+                  leading: Icon(Icons.camera),
+                  title: Text("Scan receipt"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.corporate_fare),
+                  title: Text("Connect to bank account"),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
 
-void showBottomSheet(BuildContext context) {
-  showModalBottomSheet(
-    context: context,
-    backgroundColor: Colors.white,
-    builder: (context) {
-      return SizedBox(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Wrap(
-            children: [
-              Center(
-                child: Container(
-                  width: 40,
-                  height: 4,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[800],
-                    borderRadius: BorderRadius.circular(2),
-                  ),
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.edit),
-                title: Text("Add expenses manually"),
-                onTap: () => addExpense(context),
-              ),
-              ListTile(
-                leading: Icon(Icons.camera),
-                title: Text("Scan receipt"),
-              ),
-              ListTile(
-                leading: Icon(Icons.corporate_fare),
-                title: Text("Connect to bank account"),
-              ),
-            ],
+class ExpenseListView extends StatelessWidget {
+  const ExpenseListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = context.watch<ExpensesViewModel>();
+    return ListView.separated(
+      itemBuilder: (context, index) {
+        final expense = viewModel.expenses[index];
+        return Dismissible(
+          key: ValueKey(expense.id),
+          direction: DismissDirection.endToStart,
+          onDismissed: (direction) {
+            viewModel.removeExpense(expense);
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text("Expense removed")));
+          },
+          background: Container(
+            color: Colors.redAccent,
+            alignment: Alignment.centerRight,
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: const Icon(Icons.delete),
           ),
-        ),
-      );
-    },
-  );
+          child: ListTile(
+            title: Text(
+              "\$${viewModel.expenses[index].amount.toStringAsFixed(2)}",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            subtitle: Text(viewModel.expenses[index].title),
+            trailing: Text(
+              "${viewModel.expenses[index].category.name} \n${viewModel.expenses[index].date.toLocal().toIso8601String().split('T').first}",
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (context, index) => const Divider(height: 0.5),
+      itemCount: viewModel.expenses.length,
+    );
+  }
 }
 
-void addExpense(BuildContext context) {
-  Navigator.pop(context);
-  Navigator.of(
-    context,
-  ).push(MaterialPageRoute(builder: (context) => AddExpense()));
+class EmptyView extends StatelessWidget {
+  final Function(BuildContext) showBottomSheet;
+  const EmptyView({super.key, required this.showBottomSheet});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 20.0,
+      children: [
+        Center(
+          child: Image.asset(
+            'assets/images/empty.png',
+            width: MediaQuery.of(context).size.width / 1.5,
+            fit: BoxFit.fitWidth,
+          ),
+        ),
+        Center(
+          child: Text(
+            "No expenses yet",
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+        Center(
+          child: ElevatedButton.icon(
+            onPressed: () {
+              showBottomSheet(context);
+            },
+            icon: Icon(Icons.add),
+            label: Text("Add your first expense"),
+            style: ElevatedButton.styleFrom(
+              // textStyle: Theme.of(context).textTheme.labelLarge,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
